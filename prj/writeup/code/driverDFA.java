d1cc9da799ffe301caa1d3da3cd785f3e84c6c69
ConnorFitz429 <42756858+ConnorFitz429@users.noreply.github.com>
2020-05-13 01:35:40 -0400

    Update driverDFA.java

--- prj/writeup/code/driverDFA.java
+++ prj/writeup/code/driverDFA.java
@@ -13,6 +13,7 @@
 
 
 import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class driverDFA {
@@ -22,14 +23,24 @@ public class driverDFA {
     Scanner keyboard = new Scanner(System.in);
 		
     // the while loop below allows for the user to either quit the program or retry
-	  while(i == 0) {
-	    BuddySystem m = new BuddySystem();
-	    m.reset();
-	    m.process();
-	    System.out.println();
-	    System.out.println("Would you like to restart?");
-	    System.out.println("Yes: 0    No: 1");
-	    i = keyboard.nextInt();
+	while(i == 0) {
+	  BuddySystem m = new BuddySystem();
+	  m.reset();
+	  m.process();
+	  System.out.println();
+	  System.out.println("Would you like to restart?");
+	  System.out.println("Yes: 0    No: 1");
+	  try {
+		  i = keyboard.nextInt();
+	  } // try()
+	  catch (InputMismatchException exception) {
+		  System.out.println("");
+		  System.out.println("Please enter '0' or '1'");
+		  keyboard.next();
+      } catch(ArrayIndexOutOfBoundsException ex) {
+		System.out.println("");
+	    System.out.println("Please enter '0' or '1'");
+	  } // catch()
     } // while
   } // main()
 } // driverDFA
